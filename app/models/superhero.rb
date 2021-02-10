07977e76e9ae410cb90288fa99057caea2b60ca7
Christerle Nounoute <christerlesn@gmail.com>
2021-02-10 16:39:25 +0000

    Automatically backed up by Learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,16 +1,16 @@
 class SuperHero
   attr_reader :name, :power, :biography
 
-  @@all = []
+  SUPERHEROS = []
 
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @biography = params[:biography]
-    @@all << self
+    SUPERHEROS << self
   end
 
   def self.all
-    @@all
+    SUPERHEROS
   end
 end
